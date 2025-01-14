8d20e52bbefa855c9bb7cecba6e19796042ce02f
localworkarea <localworkarea@gmail.com>
2025-01-14 16:41:07 +0200

    up

--- src/js/app.js
+++ src/js/app.js
@@ -60,7 +60,7 @@ flsFunctions.spollers();
 Документація: https://template.fls.guru/template-docs/modul-pokazat-eshhjo.html
 Сніппет (HTML): showmore
 */
-// flsFunctions.showMore();
+flsFunctions.showMore();
 
 /*
 Модуль "До/Після"
